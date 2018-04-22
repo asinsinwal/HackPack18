39b7869c5f7349bcedbb4171d2e2a8c2c943021d
asinsinwal <assinsin@ncsu.edu>
2018-04-21 23:34:14 -0400

    Concept implemented

--- background.js
+++ background.js
@@ -1,12 +1,7 @@
 // Saves the url of the current active tab
 var activeTab = "";
 var day;
-
-// Variables to store time spent on each website
-var timeOnFacebook;
-var timeOnWhatsApp;
-var timeOnYouTube;
-var timeOnNetflix;
+var activeUser = "";
 
 // Websites to track
 var websitesTracked;
@@ -28,8 +23,7 @@ chrome.runtime.onInstalled.addListener(function(params){
 
         // Set the global values
         websitesTracked = ["facebook.com","netflix.com","web.whatsapp.com","youtube.com"];
-        timeOnFacebook = timeOnNetflix = timeOnWhatsApp = timeOnYouTube = 0;
-        timeOnWebsites = [timeOnFacebook,timeOnNetflix,timeOnWhatsApp,timeOnYouTube];
+        timeOnWebsites = [0,0,0,0];
         day = new Date();
 
         // assign key-value to json object
@@ -50,27 +44,131 @@ chrome.runtime.onInstalled.addListener(function(params){
 chrome.storage.onChanged.addListener(function(){
     chrome.storage.local.get("trackWebsites", function(json_obj){
         websitesTracked = JSON.parse(json_obj.trackWebsites);
+        console.log(JSON.parse(json_obj.timeOnWebsites));
     });
 });
 
+initiate();
 
 // Start the extension
 function initiate(){
+    
     chrome.storage.local.get(null, function(json_obj){
         day = json_obj["dayTime"];
-        websitesTracked = json_obj.trackWebsites;
-        timeOnWebsites = json_obj.timeOnWebsites; 
+        websitesTracked = JSON.parse(json_obj.trackWebsites);
+        timeOnWebsites = JSON.parse(json_obj.timeOnWebsites); 
     });
 
+    // Get current active url
+    getActiveUrl();
+
+    // Handle Multiple events on regular interval
+    handleEvents();
     /*
-    BAAKI HAI
+        chrome.tabs.onActivated.addListener(function() {
+            getActiveUrl();
+        });
+        chrome.tabs.onUpdated.addListener(function(tab) {
+            getActiveUrl();
+        });
+        chrome.windows.onFocusChanged.addListener(function(value) {
+            // value of window ID
+            if (value === chrome.windows.WINDOW_ID_NONE) {
+                activeUser = false;
+            } else {
+                activeUser = true;
+            }
+            getActiveUrl();
+        });
     */
-   
+    activeUser = true;
+
+    // Update the storage values
+
 }
 
-// Reset the values
-function reset(){
-    /*
-    BAAKI HAI
-    */
+// Set the interval duration
+function setDurationListener(){
+    if(activeTab != null && activeTab != ""){
+        var domain = getDomain(activeTab);
+        if(activeUser){
+            if(checkDomainInList(domain)){
+                var index = getIndexOfDomain(domain);
+                timeOnWebsites[index] += 1;
+                console.log("Time changed for domain: " + domain);
+                console.log("Tracked Time Array: " + JSON.stringify(timeOnWebsites));
+            }
+        }
+    }
+}
+
+// Get the active url
+function getActiveUrl(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(values){
+        if(values.length > 0){
+            activeTab = values[0].url;
+            console.log("Active URL: " + activeTab);
+        } else {
+            activeTab = null;
+        }
+    });
+}
+
+// Handling tab events
+function handleEvents() {
+    chrome.tabs.onActivated.addListener(function() {
+        getActiveUrl();
+ //Debug        console.log("NN: Some tab activated");
+    });
+    chrome.tabs.onUpdated.addListener(function(tab) {
+        getActiveUrl();
+ //Debug          console.log("NN: Some tab updated");
+    });
+    chrome.windows.onFocusChanged.addListener(function(value) {
+        // value of window ID
+ //Debug        console.log("NN: Some window focus changed");
+        if (value === chrome.windows.WINDOW_ID_NONE) {
+            activeUser = false;
+        } else {
+            activeUser = true;
+        }
+        getActiveUrl();
+    });
+
+    //set interval of 1s
+    window.setInterval(setDurationListener, 1000);
+}
+
+// To get the domain from active url
+function getDomain(value){
+    var parts = value.split(":\/\/");
+    if(parts.length <= 1){
+        value = parts[0];
+    } else {
+        value = parts[1];
+    }
+    value = value.replace(/www\./g,'');
+
+    var result = value.split('\/')[0];
+ //Debug    console.log("Domain " + result);
+    return result;
+}
+
+
+// Check if domain is in the tracked websites array
+function checkDomainInList(value){
+    //console.log("Check Domain Value: " + $.inArray(value, websitesTracked));
+    //return ($.inArray(value, websitesTracked) > -1);
+    for(var i = 0; i < websitesTracked.length; i++){
+        if(websitesTracked[i] === value){
+ //Debug            console.log("Domain found");
+            return true;
+        }
+    }
+    return false;
+}
+
+// Get index of the tracked website
+function getIndexOfDomain(value){
+    return websitesTracked.indexOf(value);
 }
